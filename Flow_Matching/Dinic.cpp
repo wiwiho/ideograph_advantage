ec9aebed6c5c27ee105b070f3a37cbbe438be169
8e7 <justinlai03@yahoo.com.tw>
2022-05-20 16:24:23 +0800

    Added dinic

---
+++ Flow_Matching/Dinic.cpp
@@ -0,0 +1,60 @@
+struct MaxFlow { // 1-base
+	struct edge {
+		int to, cap, flow, rev;
+	};
+	vector<edge> g[MAXN];
+	int s, t, dis[MAXN], ind[MAXN], n;
+
+	void init(int _n) {
+		n = _n + 2;
+		s = _n + 1, t = _n + 2;
+		for (int i = 0; i <= n; ++i) g[i].clear();
+	}
+	void reset() {
+		for (int i = 0; i <= n; ++i)
+			for (auto &j : g[i]) j.flow = 0;
+	}
+	void add_edge(int u, int v, int cap) {
+		g[u].pb(edge{v, cap, 0, (int)g[v].size()});
+		g[v].pb(edge{u, 0, 0, (int)g[u].size() - 1});
+	}
+	bool bfs() {
+		fill(dis, dis+n+1, -1);
+		queue<int> q;
+		q.push(s), dis[s] = 0;
+		while (!q.empty()) {
+			int cur = q.front(); q.pop();
+			for (auto &e : g[cur]) {
+				if (dis[e.to] != -1 && e.flow != e.cap) {
+					q.push(e.to);
+					dis[e.to] = dis[cur] + 1;
+				}
+			}
+		}
+		return dis[t] != -1;
+	}
+	int dfs(int u, int cap) {
+		if (u == t || !cap) return cap;
+		for (int &i = ind[u]; i < (int)g[u].size(); ++i) {
+			edge &e = g[u][i];
+			if (dis[e.to] == dis[u] + 1 && e.flow != e.cap) {
+				int df = dfs(e.to, min(e.cap - e.flow, cap));
+				if (df) {
+					e.flow += df;
+					g[e.to][e.rev].flow -= df;
+					return df;
+				}
+			}
+		}
+		dis[u] = -1;
+		return 0;
+	}
+	int maxflow() {
+		int flow = 0, df;
+		while (bfs()) {
+			fill(ind, ind+n+1, 0);
+			while ((df = dfs(s, INF))) flow += df;
+		}
+		return flow;
+	}
+}flow;
