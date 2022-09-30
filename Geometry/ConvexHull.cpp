9b9d35de4a43d9178a572c60811fc11d72c026a1
wiwiho <wiwiho1211@gmail.com>
2022-10-01 00:20:37 +0800

    Update Geometry

---
+++ Geometry/ConvexHull.cpp
@@ -0,0 +1,20 @@
+vector<int> getConvexHull(vector<Pt>& pts){
+    vector<int> id(SZ(pts));
+    iota(iter(id), 0);
+    sort(iter(id), [&](int x, int y){ return pts[x] < pts[y]; });
+    vector<int> hull;
+    for(int tt = 0; tt < 2; tt++){
+        int sz = SZ(hull);
+        for(int j : id){
+            Pt p = pts[j];
+            while(SZ(hull) - sz >= 2 && 
+                    cross(pts[hull.back()] - pts[hull[SZ(hull) - 2]], 
+                            p - pts[hull[SZ(hull) - 2]]) <= 0)
+                hull.pop_back();
+            hull.pb(j);
+        }
+        hull.pop_back();
+        reverse(iter(id));
+    }
+    return hull;
+}
