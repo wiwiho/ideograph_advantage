30438c2fa59e40f02848e17082864a5098a26045
wiwiho <wiwiho1211@gmail.com>
2022-10-14 13:37:43 +0800

    Reindent

--- Misc/Fraction.cpp
+++ Misc/Fraction.cpp
@@ -1,12 +1,12 @@
 struct Frac{ 
-    ll p, q;  // p / q
-    Frac(ll _p, ll _q): p(_p), q(_q) { if(q < 0) p = -p, q = -q; }
+	ll p, q;  // p / q
+	Frac(ll _p, ll _q): p(_p), q(_q) { if(q < 0) p = -p, q = -q; }
 };
 Frac operator-(Frac a) { return Frac(-a.p, a.q); }
 Frac operator+(Frac a, Frac b){
-    ll q = a.q * b.q;
-    ll p = a.p * b.q + b.p * a.q;
-    return Frac(p, q);
+	ll q = a.q * b.q;
+	ll p = a.p * b.q + b.p * a.q;
+	return Frac(p, q);
 }
 Frac inv(Frac a){ return Frac(a.q, a.p); }
 Frac operator-(Frac a, Frac b) { return a + (-b); }
