2a0dbe4f55f6789a6f037ed91eb475fbc5db1698
wiwiho <wiwiho1211@gmail.com>
2023-03-19 21:03:40 +0800

    Update template and vimrc

--- Basic/template.cpp
+++ Basic/template.cpp
@@ -1,28 +1,36 @@
 //Challenge: Accepted
-#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
+
+#define io ios_base::sync_with_stdio(0);cin.tie(0)
+#define iter(v) v.begin(),v.end()
+#define SZ(v) (int)v.size()
+#define pb emplace_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
 #ifdef zisk
 void debug(){cout << endl;}
-template<class T, class ... U> void debug(T a, U ... b){cout << a << " ", debug(b...);}
-template<class T> void pary(T l, T r) {
+template<class T, class ... U>
+void debug(T a, U ... b){cout << a << " ", debug(b...);}
+template<class T> void pary(T l, T r){
 	while (l != r) cout << *l << " ", l++;
 	cout << endl;
 }
 #else
-#define debug(...) 0
-#define pary(...) 0
+#define debug(...) void()
+#define pary(...) void()
 #endif
-#define ll long long
-#define maxn 50005
-#define pii pair<int, int>
-#define ff first
-#define ss second
-#define io ios_base::sync_with_stdio(0);cin.tie(0);
-#define iter(v) v.begin(),v.end()
-#define SZ(v) (int)v.size()
-#define pb emplace_back
-int main() {
-	io
-	
+template<class A, class B>
+ostream& operator<<(ostream& o, pair<A,B> p)
+    { return o << '(' << p.ff << ',' << p.ss << ')'; }
+
+int main(){
+	io;
 }
