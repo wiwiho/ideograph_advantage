5cd1822838d9167dae217f8b8c72141ace158f43
wiwiho <wiwiho1211@gmail.com>
2023-06-22 00:27:33 +0800

    Test workflow

--- scripts/install_consolas.sh
+++ scripts/install_consolas.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p /tmp/fonttemp
 cd /tmp/fonttemp
 
-wget https://web.archive.org/web/20171225132744/http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
+wget https://web.archive.org/web/20171225132744/http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe --no-check-certificate
 
 cabextract -F ppviewer.cab PowerPointViewer.exe
 cabextract -L -F "CONSOL*.TTF" ppviewer.cab
