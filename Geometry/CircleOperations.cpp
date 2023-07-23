74542208aba9a969f3d9a06ccf000087872fd954
wiwiho <wiwiho1211@gmail.com>
2023-07-23 21:43:12 +0800

    Update CCtang

--- Geometry/CircleOperations.cpp
+++ Geometry/CircleOperations.cpp
@@ -46,7 +46,7 @@ bool CCinter(Cir &a, Cir &b, pdd &p1, pdd &p2) {
 vector<Line> CCtang( const Cir& c1 , const Cir& c2 , int sign1 ){
   vector<Line> ret;
   double d_sq = abs2( c1.O - c2.O );
-  if (sign(d_sq) == 0) return ret;
+  if (sgn(d_sq) == 0) return ret;
   double d = sqrt(d_sq);
   pdd v = (c2.O - c1.O) / d;
   double c = (c1.R - sign1 * c2.R) / d; // cos t
@@ -57,8 +57,8 @@ vector<Line> CCtang( const Cir& c1 , const Cir& c2 , int sign1 ){
         v.Y * c + sign2 * h * v.X);
     pdd p1 = c1.O + n * c1.R;
     pdd p2 = c2.O + n * (c2.R * sign1);
-    if (sign(p1.X - p2.X) == 0 and
-        sign(p1.Y - p2.Y) == 0)
+    if (sgn(p1.X - p2.X) == 0 and
+        sgn(p1.Y - p2.Y) == 0)
       p2 = p1 + perp(c2.O - c1.O);
     ret.pb(Line(p1, p2));
   }
