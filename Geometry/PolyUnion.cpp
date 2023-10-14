bffa407c5b578101089ca1695333bb135aa8e55c
wiwiho <wiwiho1211@gmail.com>
2023-10-14 16:31:31 +0800

    Update geometry

---
+++ Geometry/PolyUnion.cpp
@@ -0,0 +1,37 @@
+// from 8BQube
+ld rat(pll a, pll b) {
+  return sgn(b.X) ? (ld)a.X / b.X : (ld)a.Y / b.Y;
+} // all poly. should be ccw
+ld polyUnion(vector<vector<pll>> &poly) {
+  ld res = 0;
+  for (auto &p : poly)
+    for (int a = 0; a < SZ(p); ++a) {
+      pll A = p[a], B = p[(a + 1) % SZ(p)];
+      vector<pair<ld, int>> segs = {{0, 0}, {1, 0}};
+      for (auto &q : poly) {
+        if (&p == &q) continue;
+        for (int b = 0; b < SZ(q); ++b) {
+          pll C = q[b], D = q[(b + 1) % SZ(q)];
+          int sc = ori(A, B, C), sd = ori(A, B, D);
+          if (sc != sd && min(sc, sd) < 0) {
+            ld sa = cross(D - C, A - C), sb = cross(D - C, B - C);
+            segs.pb(sa / (sa - sb), sgn(sc - sd));
+          }
+          if (!sc && !sd && &q < &p && sgn(dot(B - A, D - C)) > 0) {
+            segs.pb(rat(C - A, B - A), 1);
+            segs.pb(rat(D - A, B - A), -1);
+          }
+        }
+      }
+      sort(iter(segs));
+      for (auto &s : segs) s.X = clamp(s.X, 0.0, 1.0);
+      ld sum = 0;
+      int cnt = segs[0].second;
+      for (int j = 1; j < SZ(segs); ++j) {
+        if (!cnt) sum += segs[j].X - segs[j - 1].X;
+        cnt += segs[j].Y;
+      }
+      res += cross(A, B) * sum;
+    }
+  return res / 2;
+}
