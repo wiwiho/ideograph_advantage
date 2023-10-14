b8a1a6586992fb0f16a0682f97db44cba2f5a69b
wiwiho <wiwiho1211@gmail.com>
2023-10-14 16:46:23 +0800

    Add PBDS Tree

---
+++ Basic/pbds.cpp
@@ -0,0 +1,5 @@
+#include <bits/extc++.h>
+using namespace __gnu_pbds;
+using Tree = tree<int, null_type, less<>, rb_tree_tag, tree_order_node_statistics_update>;
+// .find_by_order(x)
+// .order_of_key(x)
