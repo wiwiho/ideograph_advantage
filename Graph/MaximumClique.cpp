11075ead69768d140da02db99ebec8c0ace70520
wiwiho <wiwiho1211@gmail.com>
2023-11-04 11:47:53 +0800

    Update maximum/maximal clique

--- Graph/MaximumClique.cpp
+++ Graph/MaximumClique.cpp
@@ -1,51 +1,50 @@
-const int MAXN = 40;
-typedef bitset<MAXN> bst;
-struct Maximum_Clique {
-  bst N[MAXN], empty;
-  int p[MAXN], n;
-  bst ans;
-  // find all maximal clique
-  void BronKerbosch2(bst R, bst P, bst X) {
-    if (P == empty && X == empty){
-      if(ans.count() < R.count()) ans = R;
-      return;
-    }
-    bst tmp = P | X;
-    int u;
-    if ((R | P | X).count() <= ans.count()) return;
-    for (int uu = 0; uu < n; ++uu) {
-      u = p[uu];
-      if (tmp[u] == 1) break;
-    }
-    // if (double(clock())/CLOCKS_PER_SEC > .999)
-    // return;
-    bst now2 = P & ~N[u];
-    for (int vv = 0; vv < n; ++vv) {
-      int v = p[vv];
-      if (now2[v] == 1) {
-        R[v] = 1;
-        BronKerbosch2(R, P & N[v], X & N[v]);
-        R[v] = 0, P[v] = 0, X[v] = 1;
-      }
-    }
-  }
+struct MaxClique { // fast when N <= 100
+  bitset<N> G[N], cs[N];
+  int ans, sol[N], q, cur[N], d[N], n;
   void init(int _n) {
     n = _n;
-    for (int i = 0; i < n; ++i) N[i].reset();
+    for (int i = 0; i < n; ++i) G[i].reset();
   }
-  void add_edge(int u, int v) {
-    N[u][v] = N[v][u] = 1;
+  void add_edge(int u, int v) { 
+    G[u][v] = G[v][u] = 1;
   }
-  void complement(){
-    for(int i = 0; i < n; i++)
-      for(int j = 0; j < n; j++)
-        if(i != j) N[i][j] = !N[i][j];
+  void pre_dfs(vector<int> &r, int l, bitset<N> mask) {
+    if (l < 4) {
+      for (int i : r) d[i] = (G[i] & mask).count();
+      sort(ALL(r), [&](int x, int y) { return d[x] > d[y]; });
+    }
+    vector<int> c(SZ(r));
+    int lft = max(ans - q + 1, 1), rgt = 1, tp = 0;
+    cs[1].reset(), cs[2].reset();
+    for (int p : r) {
+      int k = 1;
+      while ((cs[k] & G[p]).any()) ++k;
+      if (k > rgt) cs[++rgt + 1].reset();
+      cs[k][p] = 1;
+      if (k < lft) r[tp++] = p;
+    }
+    for (int k = lft; k <= rgt; ++k)
+      for (int p = cs[k]._Find_first(); p < N; p = cs[k]._Find_next(p))
+        r[tp] = p, c[tp] = k, ++tp; 
+    dfs(r, c, l + 1, mask);
   }
-  void solve() {
-    bst R, P, X;
-    ans = 0, P.flip();
-    for (int i = 0; i < n; ++i) p[i] = i;
-    mt19937 rng(123123);
-    shuffle(p, p + n, rng), BronKerbosch2(R, P, X);
+  void dfs(vector<int> &r, vector<int> &c, int l, bitset<N> mask) {
+    while (!r.empty()) {
+      int p = r.back();
+      r.pop_back(), mask[p] = 0;
+      if (q + c.back() <= ans) return;
+      cur[q++] = p;
+      vector<int> nr;
+      for (int i : r) if (G[p][i]) nr.pb(i);
+      if (!nr.empty()) pre_dfs(nr, l, mask & G[p]); 
+      else if (q > ans) ans = q, copy_n(cur, q, sol);
+      c.pop_back(), --q;
+    }
+  }
+  int solve() {
+    vector<int> r(n);
+    ans = q = 0, iota(ALL(r), 0);
+    pre_dfs(r, 0, bitset<N>(string(n, '1')));
+    return ans;
   }
 };
