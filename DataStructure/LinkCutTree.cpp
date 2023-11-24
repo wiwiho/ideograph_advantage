70dafb591cb166da44bf37a5857b054eefdc4a55
wiwiho <wiwiho1211@gmail.com>
2023-11-24 13:42:04 +0800

    Update LCT

--- DataStructure/LinkCutTree.cpp
+++ DataStructure/LinkCutTree.cpp
@@ -1,109 +1,83 @@
-struct Splay { // subtree-sum, path-max
-	static Splay nil;
-	Splay *ch[2], *f;
-	int val, rev, size, vir, id, type;
-	pii ma;
-	Splay(int _val = 0, int _id = 0)
-		: val(_val), rev(0), size(0), vir(0), id(_id) {
-			ma = make_pair(val, id);
-			f = ch[0] = ch[1] = &nil;
-			type = 0;
-		}
-	bool isr() { //is root
-		return f->ch[0] != this && f->ch[1] != this;
-	}
-	int dir() { return f->ch[0] == this ? 0 : 1; }
-	void setCh(Splay *c, int d) {
-		ch[d] = c;
-		if (c != &nil) c->f = this;
-		pull();
-	}
-	void push() {
-		if (!rev) return;
-		swap(ch[0], ch[1]);
-		if (ch[0] != &nil) ch[0]->rev ^= 1;
-		if (ch[1] != &nil) ch[1]->rev ^= 1;
-		rev = 0;
-	}
-	void pull() {
-		// take care of the nil!
-		size = ch[0]->size + ch[1]->size + vir + type;
-		ma = max(make_pair(val, id), max(ch[0]->ma, ch[1]->ma));
-		if (ch[0] != &nil) ch[0]->f = this;
-		if (ch[1] != &nil) ch[1]->f = this;
-	}
+struct Splay { // LCT + PATH add 
+  static Splay nil;
+  Splay *ch[2], *f;
+  int rev;
+  int sz;
+  ll val, sum, tag;
+  Splay() : rev(0), sz(1), val(1), sum(1), tag(0) {
+    f = ch[0] = ch[1] = &nil;
+  }
+  bool isr() { return f->ch[0] != this && f->ch[1] != this; }
+  int dir() { return f->ch[0] == this ? 0 : 1; }
+  void setCh(Splay *c, int d) {
+    ch[d] = c;
+    if (c != &nil) c->f = this;
+    pull();
+  }
+  void push() {
+    for(int i = 0; i < 2; i++){
+      if(ch[i] == &nil) continue;
+      if(rev) swap(ch[i]->ch[0], ch[i]->ch[1]), ch[i]->rev ^= 1;
+      if(tag != 0){
+        ch[i]->tag += tag;
+        ch[i]->val += tag;
+        ch[i]->sum += tag * ch[i]->sz;
+      }
+    }
+    tag = 0;
+    rev = 0;
+  }
+  void pull() {
+    // take care of the nil!
+    sz = 1;
+    sum = val;
+    for(int i = 0; i < 2; i++){
+      if(ch[i] == &nil) continue;
+      ch[i]->f = this;
+      sz += ch[i]->sz;
+      sum += ch[i]->sum;
+    }
+  }
+  void rotate(){
+    Splay *p = f;
+    int d = dir();
+    if (!p->isr()) p->f->setCh(this, p->dir());
+    else f = p->f;
+    p->setCh(ch[!d], d);
+    setCh(p, !d);
+    p->pull(), pull();
+
+  }
+  void update(){
+    if(f != &nil) f->update();
+    push();
+  }
+  void splay(){
+    update();
+    for(Splay* fa; fa = f, !isr(); rotate())
+      if(!fa->isr()) (fa->dir() == dir() ? fa : this)->rotate();
+  }
+  Splay *access(Splay* q = &nil){
+    splay();
+    setCh(q, 1);
+    pull();
+    if (f != &nil) return f->access(this);
+    else return q;
+  }
+  void root_path(){access(), splay();}
+  void chroot() {root_path(), swap(ch[0], ch[1]), rev = 1, push(), pull();}
+  void split(Splay* y){chroot(), y->root_path();}
+  void link(Splay* y){root_path(), y->chroot(), setCh(y, 1);}
+  void cut(Splay* y) {split(y), y->push(), y->ch[0] = y->ch[0]->f = &nil;}
+  Splay *get_root(){
+    root_path();
+    auto q = this;
+    for(; q->ch[0] != &nil; q = q->ch[0]) q->push();
+    return q;
+  }
+  Splay *lca(Splay* y){
+    access(), y->root_path();
+    return y->f == &nil ? &nil : y->f;
+  }
+  bool conn(Splay* y){return get_root() == y->get_root();}
 } Splay::nil;
-Splay *nil = &Splay::nil;
-void rotate(Splay *x) {
-	Splay *p = x->f;
-	int d = x->dir();
-	if (!p->isr()) p->f->setCh(x, p->dir());
-	else x->f = p->f;
-	p->setCh(x->ch[!d], d);
-	x->setCh(p, !d);
-	p->pull(), x->pull();
-}
-void splay(Splay *x) {
-	vector<Splay *> splayVec;
-	for (Splay *q = x;; q = q->f) {
-		splayVec.pb(q);
-		if (q->isr()) break;
-	}
-	reverse(iter(splayVec));
-	for (auto it : splayVec) it->push();
-	while (!x->isr()) {
-		if (x->f->isr()) rotate(x);
-		else if (x->dir() == x->f->dir())
-			rotate(x->f), rotate(x);
-		else rotate(x), rotate(x);
-	}
-}
-Splay *access(Splay *x) {
-	Splay *q = nil;
-	for (; x != nil; x = x->f){
-		splay(x);
-		x->vir -= q->size; x->vir += x->ch[1]->size;
-		x->setCh(q, 1); x->pull();
-		q = x;
-	}
-	return q;
-}
-void root_path(Splay *x) { access(x), splay(x); }
-void chroot(Splay *x) {
-	root_path(x), x->rev ^= 1;
-	x->push(), x->pull();
-}
-void split(Splay *x, Splay *y) {
-	chroot(x), root_path(y);
-}
-void link(Splay *x, Splay *y) {
-	chroot(x), root_path(y);
-	x->f = y; y->vir += x->size;
-}
-void cut(Splay *x, Splay *y) {
-	split(x, y);
-	y->push();
-	y->ch[0] = y->ch[0]->f = nil;
-	y->pull();
-}
-Splay *get_root(Splay *x) {
-	for (root_path(x); x->ch[0] != nil; x = x->ch[0])
-		x->push();
-	splay(x);
-	return x;
-}
-bool conn(Splay *x, Splay *y) {
-	return get_root(x) == get_root(y);
-}
-Splay *lca(Splay *x, Splay *y) {
-	access(x), root_path(y);
-	if (y->f == nil) return y;
-	return y->f;
-}
-void change(Splay *x, int val) {
-	splay(x), x->val = val, x->pull();
-}
-pii query(Splay *x, Splay *y) {
-	split(x, y);
-	return y->ma;
-}
