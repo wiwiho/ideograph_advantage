5d84a9c05390a6434e01eab30279f4af138440b3
wiwiho <wiwiho1211@gmail.com>
2024-02-09 22:09:59 +0800

    Update flow

--- Flow_Matching/Dinic.cpp
+++ Flow_Matching/Dinic.cpp
@@ -1,61 +1,63 @@
-struct MaxFlow { // 1-base
-	struct edge {
-		int to, cap, flow, rev;
-	};
-	vector<edge> g[maxn];
-	int s, t, dis[maxn], ind[maxn], n;
+struct Dinic { // 0-based, O(V^2E), unit flow: O(min(V^{2/3}E, E^{3/2})), bipartite matching: O(sqrt(V)E)
+  struct edge {
+    ll to, cap, flow, rev;
+  };
 
-	void init(int _n) {
-		n = _n + 2;
-		s = _n + 1, t = _n + 2;
-		for (int i = 0; i <= n; ++i) g[i].clear();
-	}
-	void reset() {
-		for (int i = 0; i <= n; ++i)
-			for (auto &j : g[i]) j.flow = 0;
-	}
-	void add_edge(int u, int v, int cap) {
-		g[u].pb(edge{v, cap, 0, (int)g[v].size()});
-		g[v].pb(edge{u, 0, 0, (int)g[u].size() - 1});
-		//change g[v] to cap for undirected graphs
-	}
-	bool bfs() {
-		fill(dis, dis+n+1, -1);
-		queue<int> q;
-		q.push(s), dis[s] = 0;
-		while (!q.empty()) {
-			int cur = q.front(); q.pop();
-			for (auto &e : g[cur]) {
-				if (dis[e.to] == -1 && e.flow != e.cap) {
-					q.push(e.to);
-					dis[e.to] = dis[cur] + 1;
-				}
-			}
-		}
-		return dis[t] != -1;
-	}
-	int dfs(int u, int cap) {
-		if (u == t || !cap) return cap;
-		for (int &i = ind[u]; i < (int)g[u].size(); ++i) {
-			edge &e = g[u][i];
-			if (dis[e.to] == dis[u] + 1 && e.flow != e.cap) {
-				int df = dfs(e.to, min(e.cap - e.flow, cap));
-				if (df) {
-					e.flow += df;
-					g[e.to][e.rev].flow -= df;
-					return df;
-				}
-			}
-		}
-		dis[u] = -1;
-		return 0;
-	}
-	int maxflow() {
-		int flow = 0, df;
-		while (bfs()) {
-			fill(ind, ind+n+1, 0);
-			while ((df = dfs(s, inf))) flow += df;
-		}
-		return flow;
-	}
-}flow;
+  int n, s, t;
+  vector<vector<edge>> g;
+  vector<int> dis, ind;
+
+  void init(int _n) {
+    n = _n;
+    g.assign(n, vector<edge>());
+  }
+  void reset() {
+    for (int i = 0; i < n; ++i)
+      for (auto &j : g[i]) j.flow = 0;
+  }
+  void add_edge(int u, int v, ll cap) {
+    g[u].pb(edge{v, cap, 0, SZ(g[v])});
+    g[v].pb(edge{u, 0, 0, SZ(g[u]) - 1});
+    //change g[v] to cap for undirected graphs
+  }
+  bool bfs() {
+    dis.assign(n, -1);
+    queue<int> q;
+    q.push(s), dis[s] = 0;
+    while (!q.empty()) {
+      int cur = q.front(); q.pop();
+      for (auto &e : g[cur]) {
+        if (dis[e.to] == -1 && e.flow != e.cap) {
+          q.push(e.to);
+          dis[e.to] = dis[cur] + 1;
+        }
+      }
+    }
+    return dis[t] != -1;
+  }
+  ll dfs(int u, ll cap) {
+    if (u == t || !cap) return cap;
+    for (int &i = ind[u]; i < SZ(g[u]); ++i) {
+      edge &e = g[u][i];
+      if (dis[e.to] == dis[u] + 1 && e.flow != e.cap) {
+        ll df = dfs(e.to, min(e.cap - e.flow, cap));
+        if (df) {
+          e.flow += df;
+          g[e.to][e.rev].flow -= df;
+          return df;
+        }
+      }
+    }
+    dis[u] = -1;
+    return 0;
+  }
+  ll maxflow(int _s, int _t) {
+    s = _s; t = _t;
+    ll flow = 0, df;
+    while (bfs()) {
+      ind.assign(n, 0);
+      while ((df = dfs(s, INF))) flow += df;
+    }
+    return flow;
+  }
+};
