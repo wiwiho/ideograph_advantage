24c10c31ffb307b637f8eeb096e6995eb840e9df
wiwiho <wiwiho1211@gmail.com>
2024-02-10 22:10:45 +0800

    Update matching

--- Flow_Matching/BipartiteMatching.cpp
+++ Flow_Matching/BipartiteMatching.cpp
@@ -1,7 +1,7 @@
 //min vertex cover: take all unmatched vertices in L and find alternating tree,
 //ans is not reached in L + reached in R
 // O(VE)
-int n; //1-base, max matching
+int n; // 1-based, max matching
 int mx[maxn], my[maxn];
 bool adj[maxn][maxn], vis[maxn];
 bool dfs(int u) {
@@ -17,58 +17,50 @@ bool dfs(int u) {
   return 0;
 }
 // O(E sqrt(V)), O(E log V) for random sparse graphs
-struct Bipartite_Matching { // 0-base
-  int l, r;
-  int mp[maxn], mq[maxn];
-  int dis[maxn], cur[maxn];
-  vector<int> G[maxn];
+struct BipartiteMatching { // 0-based
+  int nl, nr;
+  vector<int> mx, my, dis, cur;
+  vector<vector<int>> g;
   bool dfs(int u) {
-    for (int &i = cur[u]; i < SZ(G[u]); ++i) {
-      int e = G[u][i];
-      if (!~mq[e] || (dis[mq[e]] == dis[u] + 1 && dfs(mq[e])))
-        return mp[mq[e] = u] = e, 1;
+    for (int &i = cur[u]; i < SZ(g[u]); ++i) {
+      int e = g[u][i];
+      if (!~my[e] || (dis[my[e]] == dis[u] + 1 && dfs(my[e])))
+        return mx[my[e] = u] = e, 1;
     }
     dis[u] = -1;
     return 0;
   }
   bool bfs() {
-    int rt = 0;
+    int ret = 0;
     queue<int> q;
-    fill_n(dis, l, -1);
-    for (int i = 0; i < l; ++i)
-      if (!~mp[i])
-        q.push(i), dis[i] = 0;
+    dis.assign(nl, -1);
+    for (int i = 0; i < nl; ++i)
+      if (!~mx[i]) q.push(i), dis[i] = 0;
     while (!q.empty()) {
       int u = q.front();
       q.pop();
-      for (int e : G[u])
-        if (!~mq[e])
-          rt = 1;
-        else if (!~dis[mq[e]]) {
-          q.push(mq[e]);
-          dis[mq[e]] = dis[u] + 1;
+      for (int e : g[u])
+        if (!~my[e]) ret = 1;
+        else if (!~dis[my[e]]) {
+          q.push(my[e]);
+          dis[my[e]] = dis[u] + 1;
         }
     }
-    return rt;
+    return ret;
   }
   int matching() {
-    int rt = 0;
-    fill_n(mp, l, -1);
-    fill_n(mq, r, -1);
+    int ret = 0;
+    mx.assign(nl, -1); my.assign(nr, -1);
     while (bfs()) {
-      fill_n(cur, l, 0);
-      for (int i = 0; i < l; ++i)
-        if (!~mp[i] && dfs(i))
-          ++rt;
+      cur.assign(nl, 0);
+      for (int i = 0; i < nl; ++i)
+        if (!~mx[i] && dfs(i)) ++ret;
     }
-    return rt;
+    return ret;
   }
-  void add_edge(int s, int t) {
-    G[s].pb(t);
+  void add_edge(int s, int t) { g[s].pb(t); }
+  void init(int _nl, int _nr) {
+    nl = _nl, nr = _nr;
+    g.assign(nl, vector<int>());
   }
-  void init(int _l, int _r) {
-    l = _l, r = _r;
-    for (int i = 0; i < l; ++i)
-      G[i].clear();
-  }
-} match;
+};
