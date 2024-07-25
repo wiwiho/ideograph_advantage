f26d2175986d5c7abd0e0beeee1114920cca95df
WayneYam <dd490mp@gmail.com>
2024-07-25 12:44:04 +0800

    Golfed BostanMori a little bit and added lienar recursion solver

--- Polynomial/BostanMori.cpp
+++ Polynomial/BostanMori.cpp
@@ -4,39 +4,49 @@ NTT<262144, mod, 3> ntt;
 // size of NTT has to > 2 * d
 ll BostanMori(vector<ll> P, vector<ll> Q, long long k) {
   int d = max((int)P.size(), (int)Q.size() - 1);
-  P.resize(d, 0);
-  Q.resize(d + 1, 0); 
+  vector M = {P, Q};
+  M[0].resize(d, 0);
+  M[1].resize(d + 1, 0);
   int sz = (2 * d + 1 == 1 ? 2 : (1 << (__lg(2 * d) + 1)));
+  vector<ll> Qn(sz);
+  vector N(2, vector<ll>(sz));
   while(k) {
-    vector<ll> Qneg(sz);
-    for(int i = 0; i < (int)Q.size(); i++){
-      Qneg[i] = Q[i] * ((i & 1) ? -1 : 1);
-      if(Qneg[i] < 0) Qneg[i] += mod;
+    fill(iter(Qn), 0);
+    for(int i = 0; i < d + 1; i++){
+      Qn[i] = M[1][i] * ((i & 1) ? -1 : 1);
+      if(Qn[i] < 0) Qn[i] += mod;
     }
-    ntt(Qneg, sz, false);
+    ntt(Qn, sz, false);
 
-    vector<ll> U(sz), V(sz);
-    for(int i = 0; i < (int)P.size(); i++)
-      U[i] = P[i];
-    for(int i = 0; i < (int)Q.size(); i++)
-      V[i] = Q[i];
-
-    ntt(U, sz, false);
-    ntt(V, sz, false);
-    for(int i = 0; i < sz; i++)
-      U[i] = U[i] * Qneg[i] % mod;
-    for(int i = 0; i < sz; i++)
-      V[i] = V[i] * Qneg[i] % mod;
-    ntt(U, sz, true);
-    ntt(V, sz, true);
-
-    for(int i = k & 1; i <= 2 * d - 1; i += 2)
-      P[i >> 1] = U[i];
-    for(int i = 0; i <= 2 * d; i += 2)
-      Q[i >> 1] = V[i];
+    ll t[2] = {k & 1, 0};
+    for(int i = 0; i < 2; i++){
+      fill(iter(N[i]), 0);
+      copy(iter(M[i]), N[i].begin()); 
+      ntt(N[i], sz, false);
+      for(int j = 0; j < sz; j++)
+        N[i][j] = N[i][j] * Qn[j] % mod;
+      ntt(N[i], sz, true);
+      for(int j = t[i]; j < 2 * siz(M[i]); j += 2){
+        M[i][j >> 1] = N[i][j];
+      }
+    }
     k >>= 1;
   }
-  return P[0] * ntt.minv(Q[0]) % mod;
+  return M[0][0] * ntt.minv(M[1][0]) % mod;
 }
+ll LinearRecursion(vector<ll> a, vector<ll> c, ll k) { // a_n = \sum_{j=1}^{d} c_j a_(n-j)
+    int d = siz(a);
+    int sz = (2 * d + 1 == 1 ? 2 : (1 << (__lg(2 * d) + 1)));
 
+    c[0] = mod - 1;
+    for(ll &i : c) i = i ? mod - i : 0;
+    
+    auto A = a; A.resize(sz);
+    auto C = c; C.resize(sz);
+    ntt(A, sz, false), ntt(C, sz, false);
+    for(int i = 0; i < sz; i++) A[i] = A[i] * C[i] % mod;
+    ntt(A, sz, true);
+    A.resize(d);
 
+    return BostanMori(A, c, k);
+}
