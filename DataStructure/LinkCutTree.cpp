3892dab294ed4fb0c9e2f0215d79a6a8851530de
wiwiho <wiwiho1211@gmail.com>
2024-08-07 17:14:12 +0800

    Update LCT

--- DataStructure/LinkCutTree.cpp
+++ DataStructure/LinkCutTree.cpp
@@ -1,83 +1,91 @@
-struct Splay { // LCT + PATH add 
-  static Splay nil;
-  Splay *ch[2], *f;
-  int rev;
-  int sz;
-  ll val, sum, tag;
-  Splay() : rev(0), sz(1), val(1), sum(1), tag(0) {
-    f = ch[0] = ch[1] = &nil;
-  }
-  bool isr() { return f->ch[0] != this && f->ch[1] != this; }
-  int dir() { return f->ch[0] == this ? 0 : 1; }
-  void setCh(Splay *c, int d) {
-    ch[d] = c;
-    if (c != &nil) c->f = this;
-    pull();
-  }
-  void push() {
-    for(int i = 0; i < 2; i++){
-      if(ch[i] == &nil) continue;
-      if(rev) swap(ch[i]->ch[0], ch[i]->ch[1]), ch[i]->rev ^= 1;
-      if(tag != 0){
-        ch[i]->tag += tag;
-        ch[i]->val += tag;
-        ch[i]->sum += tag * ch[i]->sz;
-      }
+// 1-based
+template <typename Val, typename SVal> struct LCT {
+  struct node {
+    int pa, ch[2]; bool rev; int size;
+    Val v, sum, rsum; SVal sv, sub, vir;
+    node() : pa{0}, ch{0, 0}, rev{false}, size{1}, v{},
+      sum{}, rsum{}, sv{}, sub{}, vir{} {}
+  };
+#define cur o[u]
+#define lc cur.ch[0]
+#define rc cur.ch[1]
+  vector<node> o;
+  bool is_root(int u) const {
+    return o[cur.pa].ch[0]!=u && o[cur.pa].ch[1]!=u; }
+  bool is_rch(int u) const {
+    return o[cur.pa].ch[1] == u && !is_root(u); }
+  void down(int u) {
+    for (int c : {lc, rc}) if (c) {
+      if (cur.rev) set_rev(c);
     }
-    tag = 0;
-    rev = 0;
+    cur.rev = false;
   }
-  void pull() {
-    // take care of the nil!
-    sz = 1;
-    sum = val;
-    for(int i = 0; i < 2; i++){
-      if(ch[i] == &nil) continue;
-      ch[i]->f = this;
-      sz += ch[i]->sz;
-      sum += ch[i]->sum;
+  void up(int u) {
+    cur.sum = o[lc].sum + cur.v + o[rc].sum;
+    cur.rsum = o[rc].rsum + cur.v + o[lc].rsum;
+    cur.sub = cur.vir + o[lc].sub + o[rc].sub + cur.sv;
+    cur.size = o[lc].size + o[rc].size + 1;
+  }
+  void set_rev(int u) {
+    swap(lc, rc), swap(cur.sum, cur.rsum);
+    cur.rev ^= 1;
+  }
+  /* --- */
+  void rotate(int u) {
+    int f = cur.pa, g = o[f].pa, l = is_rch(u);
+    if (cur.ch[l ^ 1]) o[cur.ch[l ^ 1]].pa = f;
+    if (not is_root(f)) o[g].ch[is_rch(f)] = u;
+    o[f].ch[l] = cur.ch[l ^ 1], cur.ch[l ^ 1] = f;
+    cur.pa = g, o[f].pa = u; up(f);
+  }
+  void splay(int u) {
+    vector<int> stk = {u};
+    while (not is_root(stk.back()))
+      stk.push_back(o[stk.back()].pa);
+    while (not stk.empty())
+      down(stk.back()), stk.pop_back();
+    for (int f = cur.pa; not is_root(u); f = cur.pa) {
+      if (!is_root(f))
+        rotate(is_rch(u) == is_rch(f) ? f : u);
+      rotate(u);
     }
+    up(u);
   }
-  void rotate(){
-    Splay *p = f;
-    int d = dir();
-    if (!p->isr()) p->f->setCh(this, p->dir());
-    else f = p->f;
-    p->setCh(ch[!d], d);
-    setCh(p, !d);
-    p->pull(), pull();
-
+  void access(int x) {
+    for (int u = x, last = 0; u; u = cur.pa) {
+      splay(u);
+      cur.vir = cur.vir + o[rc].sub - o[last].sub;
+      rc = last; up(last = u);
+    }
+    splay(x);
   }
-  void update(){
-    if(f != &nil) f->update();
-    push();
+  int find_root(int u) {
+    int la = 0;
+    for (access(u); u; u = lc) down(la = u);
+    return la;
   }
-  void splay(){
-    update();
-    for(Splay* fa; fa = f, !isr(); rotate())
-      if(!fa->isr()) (fa->dir() == dir() ? fa : this)->rotate();
+  void split(int x, int y) { chroot(x); access(y); }
+  void chroot(int u) { access(u); set_rev(u); }
+  /* --- */
+  LCT(int n = 0) : o(n + 1) { o[0].size = 0; }
+  void set_val(int u, const Val &v) {
+    splay(u); cur.v = v; up(u); }
+  void set_sval(int u, const SVal &v) {
+    access(u); cur.sv = v; up(u); }
+  Val query(int x, int y) {
+    split(x, y); return o[y].sum; }
+  SVal subtree(int p, int u) {
+    chroot(p); access(u); return cur.vir + cur.sv; }
+  bool connected(int u, int v) {
+    return find_root(u) == find_root(v); }
+  void link(int x, int y) {
+    chroot(x); access(y);
+    o[y].vir = o[y].vir + o[x].sub; 
+    up(o[x].pa = y);
   }
-  Splay *access(Splay* q = &nil){
-    splay();
-    setCh(q, 1);
-    pull();
-    if (f != &nil) return f->access(this);
-    else return q;
-  }
-  void root_path(){access(), splay();}
-  void chroot() {root_path(), swap(ch[0], ch[1]), rev = 1, push(), pull();}
-  void split(Splay* y){chroot(), y->root_path();}
-  void link(Splay* y){root_path(), y->chroot(), setCh(y, 1);}
-  void cut(Splay* y) {split(y), y->push(), y->ch[0] = y->ch[0]->f = &nil;}
-  Splay *get_root(){
-    root_path();
-    auto q = this;
-    for(; q->ch[0] != &nil; q = q->ch[0]) q->push();
-    return q;
-  }
-  Splay *lca(Splay* y){
-    access(), y->root_path();
-    return y->f == &nil ? &nil : y->f;
-  }
-  bool conn(Splay* y){return get_root() == y->get_root();}
-} Splay::nil;
+  void cut(int x, int y) {
+    split(x, y); o[y].ch[0] = o[x].pa = 0; up(y); }
+#undef cur
+#undef lc
+#undef rc
+};
