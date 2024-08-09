a3e99244fa1a1a7043c9ee7ab4c680d505bdd03f
wiwiho <wiwiho1211@gmail.com>
2024-08-09 11:29:21 +0800

    Fix wrong type

--- Misc/SimulateAnnealing.cpp
+++ Misc/SimulateAnnealing.cpp
@@ -1,14 +1,14 @@
-ll anneal() {
+ld anneal() {
   mt19937 rnd_engine(seed);
-  uniform_real_distribution<ll> rnd(0, 1);
-  const ll dT = 0.001;
+  uniform_real_distribution<ld> rnd(0, 1);
+  const ld dT = 0.001;
   // Argument p
-  ll S_cur = calc(p), S_best = S_cur;
-  for (ll T = 2000; T > eps; T -= dT) {
+  ld S_cur = calc(p), S_best = S_cur;
+  for (ld T = 2000; T > eps; T -= dT) {
     // Modify p to p_prime
-    const ll S_prime = calc(p_prime);
-    const ll delta_c = S_prime - S_cur;
-    ll prob = min((ll)1, exp(-delta_c / T));
+    const ld S_prime = calc(p_prime);
+    const ld delta_c = S_prime - S_cur;
+    ld prob = min((ld)1, exp(-delta_c / T));
     if (rnd(rnd_engine) <= prob)
       S_cur = S_prime, p = p_prime;
     if (S_prime < S_best) // find min
