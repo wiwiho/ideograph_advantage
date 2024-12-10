e0b1b23ef18afc3ec1ae5adc3a807b5bcaa07ac7
wiwiho <wiwiho1211@gmail.com>
2024-12-10 11:06:28 +0800

    Minimize

--- Geometry/GeometryTemplate_full.cpp
+++ Geometry/GeometryTemplate_full.cpp
@@ -4,19 +4,33 @@ using pdd = pair<ld, ld>;
 #define Y second
 // ld eps = 1e-7;
 
-pdd operator+(pdd a, pdd b) { /*...*/ }
-pdd operator-(pdd a, pdd b) { /*...*/ }
-pdd operator*(ld i, pdd v) { /*...*/ }
-pdd operator*(pdd v, ld i) { /*...*/ }
-pdd operator/(pdd v, ld i) { /*...*/ }
-ld dot(pdd a, pdd b) { /*...*/ }
-ld cross(pdd a, pdd b) { /*...*/ }
-ld abs2(pdd v) { /*...*/ }
-ld abs(pdd v) { /*...*/ }
-int sgn(ld v) { /*...*/ }
-int ori(pdd a, pdd b, pdd c) { /*...*/ }
-bool collinearity(pdd a, pdd b, pdd c) { /*...*/ }
-bool btw(pdd p, pdd a, pdd b) { /*...*/ }
+pdd operator+(pdd a, pdd b)
+{ return {a.X + b.X, a.Y + b.Y}; }
+pdd operator-(pdd a, pdd b)
+{ return {a.X - b.X, a.Y - b.Y}; }
+pdd operator*(ld i, pdd v)
+{ return {i * v.X, i * v.Y}; }
+pdd operator*(pdd v, ld i)
+{ return {i * v.X, i * v.Y}; }
+pdd operator/(pdd v, ld i)
+{ return {v.X / i, v.Y / i}; }
+ld dot(pdd a, pdd b)
+{ return a.X * b.X + a.Y * b.Y; }
+ld cross(pdd a, pdd b)
+{ return a.X * b.Y - a.Y * b.X; }
+ld abs2(pdd v)
+{ return v.X * v.X + v.Y * v.Y; };
+ld abs(pdd v)
+{ return sqrt(abs2(v)); };
+int sgn(ld v)
+{ return v > 0 ? 1 : (v < 0 ? -1 : 0); }
+// int sgn(ld v){ return v > eps ? 1 : ( v < -eps ? -1 : 0); }
+int ori(pdd a, pdd b, pdd c)
+{ return sgn(cross(b - a, c - a)); }
+bool collinearity(pdd a, pdd b, pdd c)
+{ return ori(a, b, c) == 0; }
+bool btw(pdd p, pdd a, pdd b)
+{ return collinearity(p, a, b) && sgn(dot(a - p, b - p)) <= 0; }
 
 bool seg_intersect(pdd p1, pdd p2, pdd p3, pdd p4){
   if(btw(p1, p3, p4) || btw(p2, p3, p4) || btw(p3, p1, p2) || btw(p4, p1, p2))
--- Geometry/MinkowskiSum.cpp
+++ Geometry/MinkowskiSum.cpp
@@ -1,24 +1,18 @@
 void reorder_poly(vector<pdd>& pnts){
-	int mn = 0;
-	for(int i = 1; i < (int)pnts.size(); i++)
-		if(pnts[i].Y < pnts[mn].Y || (pnts[i].Y == pnts[mn].Y && pnts[i].X < pnts[mn].X)) 
-			mn = i;
-	rotate(pnts.begin(), pnts.begin() + mn, pnts.end());
+  int mn = 0;
+  for(int i = 1; i < (int)pnts.size(); i++)
+    if(pnts[i].Y < pnts[mn].Y || (pnts[i].Y == pnts[mn].Y && pnts[i].X < pnts[mn].X)) mn = i;
+  rotate(pnts.begin(), pnts.begin() + mn, pnts.end());
 }
-
 vector<pdd> minkowski(vector<pdd> P, vector<pdd> Q){
-	reorder_poly(P);
-	reorder_poly(Q);
-	int psz = P.size();
-	int qsz = Q.size();
-	P.pb(P[0]); P.pb(P[1]); Q.pb(Q[0]); Q.pb(Q[1]);
-	vector<pdd> ans;
-	int i = 0, j = 0;
-	while(i < psz || j < qsz){
-		ans.pb(P[i] + Q[j]);
-		int t = sgn(cross(P[i + 1] - P[i], Q[j + 1] - Q[j]));
-		if(t >= 0) i++;
-		if(t <= 0) j++;
-	}
-	return ans;
+  reorder_poly(P); reorder_poly(Q);
+  int psz = P.size(), qsz = Q.size();
+  P.pb(P[0]); P.pb(P[1]); Q.pb(Q[0]); Q.pb(Q[1]);
+  vector<pdd> ans; int i = 0, j = 0;
+  while(i < psz || j < qsz){
+    ans.pb(P[i] + Q[j]);
+    int t = sgn(cross(P[i + 1]-P[i], Q[j + 1]-Q[j]));
+    if(t >= 0) i++; if(t <= 0) j++;
+  }
+  return ans;
 }
