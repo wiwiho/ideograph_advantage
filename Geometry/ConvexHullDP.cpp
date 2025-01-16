d8db017075a1426dc7e3ec35a87fb288ba266348
wiwiho <wiwiho1211@gmail.com>
2025-01-17 02:16:19 +0800

    Retab

--- Geometry/ConvexHullDP.cpp
+++ Geometry/ConvexHullDP.cpp
@@ -1,16 +1,16 @@
 sort(iter(pts), [&](pll x, pll y) {
-    return x.Y != y.Y ? x.Y < y.Y : x.X < y.X;
-    });
+  return x.Y != y.Y ? x.Y < y.Y : x.X < y.X;
+});
 auto getvec = [&](pii x) { return pts[x.ss] - pts[x.ff]; };
 vector<pii> trans;
 for (int j = 0; j < n; j++)
-    for (int k = 0; k < n; k++)
-        if (j != k) trans.pb(pii(j, k));
+  for (int k = 0; k < n; k++)
+    if (j != k) trans.pb(pii(j, k));
 sort(iter(trans), [&](pii x, pii y) -> bool{
-    int tmp = cmp(getvec(x), getvec(y), false);
-    if (tmp != -1) return tmp;
-    pll v = getvec(x);
-    return dot(v, pts[x.ff]) > dot(v, pts[y.ff]);
+  int tmp = cmp(getvec(x), getvec(y), false);
+  if (tmp != -1) return tmp;
+  pll v = getvec(x);
+  return dot(v, pts[x.ff]) > dot(v, pts[y.ff]);
 });
 // DP for convex hull vertices (no points on edges)
 auto solve = [&](int bottom) { // O(n^3)
@@ -21,7 +21,7 @@ auto solve = [&](int bottom) { // O(n^3)
   }
   for (auto [i, j] : trans) {
     if (i <= bottom || j <= bottom ||
-            ori(pts[bottom], pts[i], pts[j]) <= 0) continue;
+        ori(pts[bottom], pts[i], pts[j]) <= 0) continue;
     // check whether i -> j is legal
     // normal trans i -> j
   }
