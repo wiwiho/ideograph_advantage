252355fb359d79f8f1b98d68ba6c792f8305a2ad
wiwiho <wiwiho1211@gmail.com>
2025-08-31 00:24:44 +0800

    Fix btw

--- Geometry/PointInConvex.cpp
+++ Geometry/PointInConvex.cpp
@@ -1,7 +1,7 @@
 bool PointInConvex(const vector<pll> &C, pll p, bool strict = true) {
   int a = 1, b = SZ(C) - 1, r = !strict;
   if (SZ(C) == 0) return false;
-  if (SZ(C) < 3) return r && btw(C[0], C.back(), p);
+  if (SZ(C) < 3) return r && btw(p, C[0], C.back());
   if (ori(C[0], C[a], C[b]) > 0) swap(a, b);
   if (ori(C[0], C[a], p) >= r || ori(C[0], C[b], p) <= -r)
     return false;
