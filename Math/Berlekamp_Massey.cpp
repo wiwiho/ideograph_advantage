ba33227864707d6aa1b3aa106abadf16e6f4f5b8
wiwiho <wiwiho1211@gmail.com>
2025-11-11 02:40:02 +0800

    add Berlekamp-Massey

---
+++ Math/Berlekamp_Massey.cpp
@@ -0,0 +1,22 @@
+// out[i] = me[0]*out[i-1] + me[1]*out[i-2]+...
+template <typename T> // modify for mod
+vector<T> BerlekampMassey(const vector<T> &output) {
+  vector<T> d(output.size() + 1), me, he;
+  for (size_t f = 0, i = 1; i <= output.size(); ++i) {
+    for (size_t j = 0; j < me.size(); ++j)
+      d[i] += output[i - j - 2] * me[j];
+    if ((d[i] -= output[i - 1]) == 0) continue;
+    if (me.empty()) {
+      me.resize(f = i);
+      continue;
+    }
+    vector<T> o(i - f - 1);
+    T k = -d[i] / d[f]; o.push_back(-k);
+    for (T x : he) o.push_back(x * k);
+    if (o.size() < me.size()) o.resize(me.size());
+    for (size_t j = 0; j < me.size(); ++j) o[j] += me[j];
+    if (i-f+he.size() >= me.size()) he = me, f = i;
+    me = o;
+  }
+  return me;
+}
