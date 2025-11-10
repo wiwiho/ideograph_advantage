88af2e24fec916f5c74b3e148dec7daef0208078
wiwiho <wiwiho1211@gmail.com>
2025-11-11 01:02:43 +0800

    update debug

--- Basic/debug.cpp
+++ Basic/debug.cpp
@@ -1,18 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define iter(v) v.begin(),v.end()
-#define SZ(v) int(v.size())
-#define pb emplace_back
-#define ff first
-#define ss second
-#define fs ff
-#define sc ss
-
-using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-
 ostream& operator<<(ostream &o, pair<auto, auto> p)
 { return o << '(' << p.ff << ',' << p.ss << ')'; }
 #ifdef zisk
@@ -27,7 +12,3 @@ void debug(auto&&... a)
 #else
 #define debug(...) void()
 #endif
-
-int main(){
-  ios_base::sync_with_stdio(false); cin.tie(0);
-}
