93ad0c693f9322039a0e12b656d167ff93d01ad6
wiwiho <wiwiho1211@gmail.com>
2025-11-11 02:46:20 +0800

    fix debug output string as array

--- Basic/debug.cpp
+++ Basic/debug.cpp
@@ -3,8 +3,10 @@ ostream& operator<<(ostream &o, pair<auto, auto> p)
 #ifdef zisk
 void debug() { cerr << "\033[0m\n"; }
 template<class T> void _d(T&& x) {
-  if constexpr (ranges::range<T>) { cerr << "{ "; 
-    for (auto&& i : x) _d(i); cerr << "} "; 
+  if constexpr (ranges::range<T> && 
+      !is_convertible_v<T, string_view>) {
+    cerr << "{ "; for (auto&& i : x) _d(i); 
+    cerr << "} "; 
   } else cerr << x << " ";
 } // ranges::subrange(l, r)
 void debug(auto&&... a) 
