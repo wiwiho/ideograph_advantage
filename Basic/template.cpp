6c0d95c1143939bcce34c8a86bc6310f3e46d01f
wiwiho <wiwiho1211@gmail.com>
2025-11-12 01:28:57 +0800

    sync full template

--- Basic/template.cpp
+++ Basic/template.cpp
@@ -13,20 +13,24 @@ using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 
-ostream& operator<<(ostream &o, pair<auto, auto> p)
-{ return o << '(' << p.ff << ',' << p.ss << ')'; }
 #ifdef zisk
-void debug() { cerr << "\033[0m\n"; }
+void debug() { cerr << "\e[0m\n"; }
 template<class T> void _d(T&& x) {
   if constexpr (ranges::range<T> && 
       !is_convertible_v<T, string_view>) {
     cerr << "{ "; for (auto&& i : x) _d(i); 
     cerr << "} "; 
+  } else if constexpr (requires { get<0>(x); }) {
+    cerr << "( ";
+    apply([](auto&&... a){ (_d(a), ...); }, x);
+    cerr << ") ";
   } else cerr << x << " ";
 } // ranges::subrange(l, r)
 void debug(auto&&... a) 
-{ cerr << "\033[33m"; (_d(a), ...); debug(); }
+{ cerr << "\e[1;33m"; (_d(a), ...); debug(); }
+#define safe debug(__PRETTY_FUNCTION__,__LINE__,"safe")
 #else
+#define safe void()
 #define debug(...) void()
 #endif
 
