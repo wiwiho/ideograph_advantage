be88bf306255a451d03c4f4d60df8ca3d9e843c5
alvingogo <accctw376@gmail.com>
2025-11-11 22:48:59 +0800

    move BM to BM

--- Polynomial/BostanMori.cpp
+++ Polynomial/BostanMori.cpp
@@ -1,3 +1,28 @@
+// out[i] = me[0]*out[i-1] + me[1]*out[i-2]+...
+vector<int> BerlekampMassey(const vector<int> &output) {
+  vector<int> d(output.size() + 1), me, he;
+  for (int f = 0, i = 1; i <= output.size(); ++i) {
+    for (int j = 0; j < me.size(); ++j)
+      add(d[i], mul(output[i - j - 2], me[j]));
+	add(d[i], -output[i-1]);
+    if (d[i] == 0) continue;
+    if (me.empty()) {
+      me.resize(f = i);
+      continue;
+    }
+    vector<int> o(i - f - 1);
+	int k = mul(d[i], -inv(d[f]));
+	o.push_back(-k);
+    for (auto x : he) o.push_back(mul(x, k));
+    if (o.size() < me.size()) o.resize(me.size());
+    for (size_t j = 0; j < me.size(); ++j) add(o[j], me[j]);
+    if (i-f+he.size() >= me.size()) he = me, f = i;
+    me = o;
+  }
+  for(auto &h: me) add(h, mod);
+  return me;
+}
+
 // Finds the k-th coefficient of P / Q in O(d log d log k)
 // size of NTT has to > 2 * d
 NTT<1<<19, 3> ntt;
@@ -31,6 +56,7 @@ int BostanMori(vector<int> P, vector<int> Q, long long k) {
   }
   return mul(M[0][0], inv(M[1][0]));
 }
+
 int LinearRecursion(vector<int> a, vector<int> c, ll k) { // a_n = \sum_{j=1}^{d} c_j a_(n-j), c_0 = 0
   int d = SZ(a), sz = 1; // 1e5, 5s
   while(sz <= 2*d) sz <<= 1;
