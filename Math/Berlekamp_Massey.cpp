8f215b2f52c1eb96d76c78272da0ddfc241fca0a
alvingogo <accctw376@gmail.com>
2025-11-11 22:33:56 +0800

    modify Berlekamp-Massey

--- Math/Berlekamp_Massey.cpp
+++ Math/Berlekamp_Massey.cpp
@@ -1,22 +1,25 @@
 // out[i] = me[0]*out[i-1] + me[1]*out[i-2]+...
-template <typename T> // modify for mod
-vector<T> BerlekampMassey(const vector<T> &output) {
-  vector<T> d(output.size() + 1), me, he;
-  for (size_t f = 0, i = 1; i <= output.size(); ++i) {
-    for (size_t j = 0; j < me.size(); ++j)
-      d[i] += output[i - j - 2] * me[j];
-    if ((d[i] -= output[i - 1]) == 0) continue;
+vector<int> BerlekampMassey(const vector<int> &output) {
+  vector<int> d(output.size() + 1), me, he;
+  for (int f = 0, i = 1; i <= output.size(); ++i) {
+    for (int j = 0; j < me.size(); ++j)
+      add(d[i], mul(output[i - j - 2], me[j]));
+	add(d[i], -output[i-1]);
+    if (d[i] == 0) continue;
     if (me.empty()) {
       me.resize(f = i);
       continue;
     }
-    vector<T> o(i - f - 1);
-    T k = -d[i] / d[f]; o.push_back(-k);
-    for (T x : he) o.push_back(x * k);
+    vector<int> o(i - f - 1);
+	int k = mul(d[i], -inv(d[f]));
+    //T k = -d[i] / d[f]; 
+	o.push_back(-k);
+    for (auto x : he) o.push_back(mul(x, k));
     if (o.size() < me.size()) o.resize(me.size());
-    for (size_t j = 0; j < me.size(); ++j) o[j] += me[j];
+    for (size_t j = 0; j < me.size(); ++j) add(o[j], me[j]);
     if (i-f+he.size() >= me.size()) he = me, f = i;
     me = o;
   }
+  for(auto &h: me) add(h, mod);
   return me;
 }
