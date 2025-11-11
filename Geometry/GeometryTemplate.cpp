352933cedb2f9e57110b40a06334aa015d8d3710
wiwiho <wiwiho1211@gmail.com>
2025-11-11 22:24:50 +0800

    update geometry

--- Geometry/GeometryTemplate.cpp
+++ Geometry/GeometryTemplate.cpp
@@ -1,24 +1,40 @@
-using ld = ll;
+using ld = double;
 using pdd = pair<ld, ld>;
 #define X first
 #define Y second
-// ld eps = 1e-7;
+const ld eps = 1e-9;
 
-pdd operator+(pdd a, pdd b) { /*...*/ }
-pdd operator-(pdd a, pdd b) { /*...*/ }
-pdd operator*(ld i, pdd v) { /*...*/ }
-pdd operator*(pdd v, ld i) { /*...*/ }
-pdd operator/(pdd v, ld i) { /*...*/ }
-ld dot(pdd a, pdd b) { /*...*/ }
-ld cross(pdd a, pdd b) { /*...*/ }
-ld abs2(pdd v) { /*...*/ }
-ld abs(pdd v) { /*...*/ }
-int sgn(ld v) { /*...*/ }
-int ori(pdd a, pdd b, pdd c) { /*...*/ }
-bool collinearity(pdd a, pdd b, pdd c) { /*...*/ }
-bool btw(pdd p, pdd a, pdd b) { /*...*/ }
+#define TEMP template<class T>
+#define ptt pair<T, T>
+TEMP ptt operator+(ptt a, ptt b)
+{ return {a.X + b.X, a.Y + b.Y}; }
+TEMP ptt operator-(ptt a, ptt b)
+{ return {a.X - b.X, a.Y - b.Y}; }
+TEMP ptt operator*(T i, ptt v)
+{ return {i * v.X, i * v.Y}; }
+TEMP ptt operator*(ptt v, T i)
+{ return {i * v.X, i * v.Y}; }
+TEMP pdd operator/(ptt v, ld i)
+{ return {v.X / i, v.Y / i}; }
+TEMP T dot(ptt a, ptt b)
+{ return a.X * b.X + a.Y * b.Y; }
+TEMP T cross(ptt a, ptt b)
+{ return a.X * b.Y - a.Y * b.X; }
+TEMP T abs2(ptt v)
+{ return v.X * v.X + v.Y * v.Y; };
+TEMP ld abs(ptt v)
+{ return sqrt(abs2(v)); };
+int sgn(ll v)
+{ return v > 0 ? 1 : (v < 0 ? -1 : 0); }
+int sgn(ld v){ return v > eps ? 1 : ( v < -eps ? -1 : 0); }
+TEMP int ori(ptt a, ptt b, ptt c)
+{ return sgn(cross(b - a, c - a)); }
+TEMP bool collinearity(ptt a, ptt b, ptt c)
+{ return ori(a, b, c) == 0; }
+TEMP bool btw(ptt p, ptt a, ptt b)
+{ return collinearity(p, a, b) && sgn(dot(a - p, b - p)) <= 0; }
 
-bool seg_intersect(pdd p1, pdd p2, pdd p3, pdd p4){
+TEMP bool seg_intersect(ptt p1, ptt p2, ptt p3, ptt p4){
   if(btw(p1, p3, p4) || btw(p2, p3, p4) || btw(p3, p1, p2) || btw(p4, p1, p2))
     return true;
   return ori(p1, p2, p3) * ori(p1, p2, p4) < 0 &&
@@ -29,12 +45,12 @@ pdd intersect(pdd p1, pdd p2, pdd p3, pdd p4){
   ld a124 = cross(p2 - p1, p4 - p1);
   return (p4 * a123 - p3 * a124) / (a123 - a124);
 }
-pdd perp(pdd p1)
-{ return pdd(-p1.Y, p1.X); }
+TEMP ptt perp(ptt p1)
+{ return {-p1.Y, p1.X}; }
 pdd projection(pdd p1, pdd p2, pdd p3)
 { return p1 + (p2 - p1) * dot(p3 - p1, p2 - p1) / abs2(p2 - p1); }
 pdd reflection(pdd p1, pdd p2, pdd p3)
-{ return p3 + perp(p2 - p1) * cross(p3 - p1, p2 - p1) / abs2(p2 - p1) * 2; }
+{ return p3 + perp(p2 - p1) * cross(p3 - p1, p2 - p1) / abs2(p2 - p1) * 2.0; }
 pdd linearTransformation(pdd p0, pdd p1, pdd q0, pdd q1, pdd r) {
   pdd dp = p1 - p0, dq = q1 - q0, num(cross(dp, dq), dot(dp, dq));
   return q0 + pdd(cross(r - p0, num), dot(r - p0, num)) / abs2(dp);
--- Geometry/HPIGeneralLine.cpp
+++ Geometry/HPIGeneralLine.cpp
@@ -1,3 +1,4 @@
+// replace corresponding functions in vector HPI
 using i128 = __int128;
 struct LN {
   ll a, b, c; // ax + by + c <= 0
@@ -11,13 +12,13 @@ pdd intersect(LN A, LN B) {
   i128 b = i128(A.c) * B.b - i128(B.c) * A.b;
   return pdd(-b / c, a / c);
 }
-bool cov(LN l, LN A, LN B) {
+bool isin(LN l, LN A, LN B) {
   i128 c = cross(A.dir(), B.dir());
   i128 a = i128(A.c) * B.a - i128(B.c) * A.a;
   i128 b = i128(A.c) * B.b - i128(B.c) * A.b;
-  return sign(a * l.b - b * l.a + c * l.c) * sign(c) >= 0;
+  return sgn(a * l.b - b * l.a + c * l.c) * sgn(c) < 0;
 }
-bool operator<(LN a, LN b) {
+int cmp(LN a, LN b) {
   if (int c = cmp(a.dir(), b.dir(), false); c != -1) return c;
   return i128(abs(b.a) + abs(b.b)) * a.c > i128(abs(a.a) + abs(a.b)) * b.c;
 }
